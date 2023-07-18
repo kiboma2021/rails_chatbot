525d96a09f4a81407bb67aacc06691c3daedc895
kiboma <omayioben@gmail.com>
2023-07-18 08:19:28 +0300

    Migrate comments schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_18_042441) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_18_051220) do
   create_table "chats", force: :cascade do |t|
     t.string "name"
     t.text "profile"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.text "body"
+    t.integer "chat_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
