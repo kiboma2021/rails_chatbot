4cb9ebd0e3cfea58108a3a5e34838b3e42dfc224
kiboma <omayioben@gmail.com>
2023-07-18 07:41:03 +0300

    Autopulate data using seed command

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,17 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+Chat.destroy_all
+
+Chat.create!([
+    {name: 'Brian', profile: 'The field of Artificial Intelligence(AI) is constantly evolving as new emerging technology developments are happening all the time. One of the latest and most revolutionary advancements is the launch of ChatGPT.
+    '},
+    {name: 'James ', profile: 'One of the latest and most revolutionary advancements of artificial intelligence is the launch of ChatGPT and later the launch to testers of Google Bard AI.'},
+    {name: 'Monica', profile: 'Furthermore, it can keep the context, allowing users to engage more naturally and making it more suitable for more difficult tasks. In addition, ChatGPT is highly secure, allowing users to rest assured in knowing that conversations they have with ChatGPT are safe. '},
+    {name: 'Sarah', profile: 'Firstly, if you’re in a leadership role, don’t ignore ChatGPT – this isn’t going to be a passing trend. Now is the time to be tuning into its capabilities, and how it can be used to leverage your business. '},
+    {name: 'Onyango ', profile: 'ChatGPT has wowed users with the depth of its functionality, swift responses, and ability to disrupt the market, making people take notice and understand the true potential of technology in business. Here are just a handful of ways I can see it being used.'},
+    {name: 'Otieno ', profile: 'Firstly, if you think it will replace your human workforce, you’re using it wrong. Completely wrong. But, I have no doubt that we will read stories in the future of businesses choosing to do exactly that. My advice? Don’t be one of them, and cherish the hard working people around you who will be the true ticket to elevating your brand. Technology and humans should be working hand in hand, rather than competing with one another for reasons such as profit margins. '},
+    {name: 'Kamau', profile: 'As an entrepreneur, too, we’re always here to support and advise should implementing this artificial intelligence tool be top of your priority list. Reach out to our team and we’d love to chat (GPT…sorry). '},
+    {name: 'Benson ', profile: 'Firstly, if you think it will replace your human workforce, you’re using it wrong. Completely wrong. But, I have no doubt that we will read stories in the future of businesses choosing to do exactly that. My advice?  '},
+    {name: 'Rodgers ', profile: 'Make sure you can navigate to your browser at localhost:3000, and if everything has gone according to plan, you should see the Rails default index page.    '},
+    {name: 'Victor', profile: 'Quick aside: Even though it’s glorious that Rails 7 doesn’t ship with Node or Webpack, there’s an unfortunate chance that you might still need that setup.  '}
+    
+
+])
