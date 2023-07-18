adddfb8a6fd093af37e7c8b5fda899a24a921c4e
kiboma <omayioben@gmail.com>
2023-07-18 08:18:33 +0300

    Add relationship

--- app/models/chat.rb
+++ app/models/chat.rb
@@ -1,4 +1,5 @@
 class Chat < ApplicationRecord
+    has_many :comments, dependent: :destroy
     validates :name, presence: true, uniqueness: true
     validates :profile, presence: true, length:{minimum: 5, maximum: 1000}
 end
